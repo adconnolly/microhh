790f90ab6f7e7eb4139c3c1e7a8a9643a94d7e60
Alex Connolly <adac@cheyenne5.cheyenne.ucar.edu>
2023-11-08 13:57:49 -0700

    Restoring drycblles test case

--- cases/drycblles_constKasNu/clean.sh
+++
@@ -1,7 +0,0 @@
-rm drycblles.default.0000000.nc
-find . -name "u.*" -not -name "*.0000000" -exec rm {} \;
-find . -name "v.*" -not -name "*.0000000" -exec rm {} \;
-find . -name "w.*" -not -name "*.0000000" -exec rm {} \;
-find . -name "th*.*" -not -name "*.0000000" -exec rm {} \;
-find . -name "time.*" -not -name "*.0000000" -exec rm {} \;
-find . -name "d*d*.*" -not -name "*.0000000" -exec rm {} \;
